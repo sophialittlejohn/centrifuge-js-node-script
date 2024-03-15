862a032fa058756b6628ebcf5971e7d361759c5f
sophian <littlejohn.sophia@gmail.com>
2024-03-15 11:20:11 -0400

    Upgrade cent-js

--- index.ts
+++ index.ts
@@ -1,25 +1,40 @@
 import "@polkadot/api-augment";
 import Centrifuge from "@centrifuge/centrifuge-js";
-import { take, firstValueFrom, switchMap, combineLatest } from "rxjs";
+import { take, firstValueFrom, switchMap, combineLatest, map } from "rxjs";
 
-const centrifuge = new Centrifuge();
-const anemoyPoolId = "4139607887";
+const centrifuge = new Centrifuge({
+  centrifugeWsUrl: "wss://fullnode.development.cntrfg.com",
+});
 
 const main = async () => {
   const api = centrifuge.getApi();
-  const results = await firstValueFrom(
-    api.pipe(
-      switchMap((api) =>
-        combineLatest([
-          api.call.poolsApi.nav(anemoyPoolId),
-          api.call.loansApi.portfolio(anemoyPoolId),
-          api.call.poolsApi.trancheTokenPrices(anemoyPoolId),
-        ])
-      ),
-      take(1)
-    )
+  const pools = await firstValueFrom(centrifuge.pools.getPools());
+  const poolIds = pools.map((pool) => pool.id);
+  const results = await Promise.all(
+    poolIds.map(async (poolId) => {
+      return firstValueFrom(
+        api.pipe(
+          switchMap((api) => {
+            return combineLatest([
+              api.call.poolsApi.nav(poolId),
+              api.call.loansApi.portfolio(poolId),
+              api.call.poolsApi.trancheTokenPrices(poolId),
+            ]);
+          }),
+          map(([nav, portfolio, trancheTokenPrices]) => {
+            return {
+              poolId: poolId,
+              nav: nav.toJSON(),
+              portfolio: portfolio.toJSON(),
+              trancheTokenPrices: trancheTokenPrices.toJSON(),
+            };
+          }),
+          take(1)
+        )
+      );
+    })
   );
-  console.log("results:", results);
+  console.log(results);
 };
 
 main().catch(console.error);
