83829f7860b47baafed83b32761a7ec46de8cbf9
sophian <littlejohn.sophia@gmail.com>
2024-03-20 16:01:58 -0400

    Add block hash call

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import "@polkadot/api-augment";
 import Centrifuge from "@centrifuge/centrifuge-js";
-import { take, firstValueFrom, switchMap, combineLatest, map } from "rxjs";
+import { firstValueFrom, switchMap, combineLatest, map } from "rxjs";
 
 const centrifuge = new Centrifuge({
   centrifugeWsUrl: "wss://fullnode.development.cntrfg.com",
@@ -19,14 +19,18 @@ const main = async () => {
               api.call.poolsApi.nav(poolId),
               api.call.loansApi.portfolio(poolId),
               api.call.poolsApi.trancheTokenPrices(poolId),
+              api.at(
+                "0xd96aba180357599933e246625ccd161c0ea90d7704e3f9e3bc67f06c040d10ac"
+              ),
             ]);
           }),
-          map(([nav, portfolio, trancheTokenPrices]) => {
+          map(([nav, portfolio, trancheTokenPrices, atCall]) => {
             return {
               poolId: poolId,
               nav: nav.toJSON(),
               portfolio: portfolio.toJSON(),
               trancheTokenPrices: trancheTokenPrices.toJSON(),
+              atCall,
             };
           })
         )
