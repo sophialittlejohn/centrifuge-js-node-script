106ce8e3a6495d1220991e70df9233be921ef61d
sophian <littlejohn.sophia@gmail.com>
2024-03-20 16:38:58 -0400

    Use api at given block hash

--- index.ts
+++ index.ts
@@ -1,42 +1,45 @@
 import "@polkadot/api-augment";
 import Centrifuge from "@centrifuge/centrifuge-js";
-import { firstValueFrom, switchMap, combineLatest, map } from "rxjs";
+import { firstValueFrom, switchMap, combineLatest, map, forkJoin } from "rxjs";
 
 const centrifuge = new Centrifuge({
   centrifugeWsUrl: "wss://fullnode.development.cntrfg.com",
 });
 
 const main = async () => {
-  const api = centrifuge.getApi();
-  const pools = await firstValueFrom(centrifuge.pools.getPools());
-  const poolIds = pools.map((pool) => pool.id);
-  const results = await Promise.all(
-    poolIds.map(async (poolId) => {
-      return firstValueFrom(
-        api.pipe(
-          switchMap((api) => {
-            return combineLatest([
-              api.call.poolsApi.nav(poolId),
-              api.call.loansApi.portfolio(poolId),
-              api.call.poolsApi.trancheTokenPrices(poolId),
-              api.at(
-                "0xd96aba180357599933e246625ccd161c0ea90d7704e3f9e3bc67f06c040d10ac"
-              ),
-            ]);
-          }),
-          map(([nav, portfolio, trancheTokenPrices, atCall]) => {
+  const poolData = centrifuge.getApi().pipe(
+    map((api) => {
+      return api.at(
+        "0xd96aba180357599933e246625ccd161c0ea90d7704e3f9e3bc67f06c040d10ac"
+      );
+    }),
+    switchMap((api) => {
+      const pools = centrifuge.pools.getPools();
+      return combineLatest([api, pools]);
+    }),
+    switchMap(([api, pools]) => {
+      const poolIds = pools.map((pool) => pool.id);
+      const poolCalls = poolIds.map((poolId) => {
+        return forkJoin([
+          api.call.poolsApi.nav(poolId),
+          api.call.loansApi.portfolio(poolId),
+          api.call.poolsApi.trancheTokenPrices(poolId),
+        ]).pipe(
+          map(([nav, portfolio, trancheTokenPrices]) => {
             return {
-              poolId: poolId,
+              poolId,
               nav: nav.toJSON(),
               portfolio: portfolio.toJSON(),
               trancheTokenPrices: trancheTokenPrices.toJSON(),
-              atCall,
             };
           })
-        )
-      );
+        );
+      });
+      return forkJoin(poolCalls);
     })
   );
+
+  const results = await firstValueFrom(poolData);
   console.log(results);
   return results;
 };
